054c85469697088c9576777720225b7bc3a7f38f
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-01-20 10:27:48 -0300

    Etapa inicial do projeto

---
+++ src/main/java/Eventfye/EventfyeApplication.java
@@ -0,0 +1,13 @@
+package Eventfye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventfyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventfyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Eventfye/EventfyeApplicationTests.java
@@ -0,0 +1,13 @@
+package Eventfye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventfyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
