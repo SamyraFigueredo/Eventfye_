40209a3cffb58adcf214873e71255c1b7b08aceb
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-01-20 11:38:19 -0300

    criando pacote model e classe Usuario

--- src/main/java/Model/Usuario.java
+++ src/main/java/Model/Usuario.java
@@ -1,2 +1,33 @@
-package Model;public class Usuario {
+package Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+
+    @Column(name = "nome_usuario", nullable = false, length = 100)
+    private String nomeUsuario;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "cpf_usuario", nullable = false, length = 14, unique = true)
+    private String cpfUsuario;
+
+    @Column(name = "email_usuario", nullable = false, length = 100, unique = true)
+    private String emailUsuario;
 }
