47de9192eb584c0f62719e6d99382077749c56ce
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-01-21 22:22:14 -0300

    criando repository e service da classe palestrante

--- src/main/java/com/webservice/eventfye/Repository/PalestranteRepository.java
+++ src/main/java/com/webservice/eventfye/Repository/PalestranteRepository.java
@@ -1,2 +1,21 @@
-package com.webservice.eventfye.Repository;public class PalestranteRepository {
+package com.webservice.eventfye.Repository;
+
+import com.webservice.eventfye.Model.Palestrante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PalestranteRepository extends JpaRepository<Palestrante, Long> {
+
+    Optional<Palestrante> findByNomeUsuario(String nome);
+
+    List<Palestrante> findByAreaExpertisePalestrante(String areaExpertise);
+    List<Palestrante> findByNomeUsuarioContaining(String nome);
+    List<Palestrante> findByNomeUsuarioStartingWith(String nome);
+    List<Palestrante> findAllByOrderByNomeUsuarioAsc();
+
+    long countByAreaExpertisePalestrante(String areaExpertise);
 }
--- src/main/java/com/webservice/eventfye/Service/PalestranteService.java
+++ src/main/java/com/webservice/eventfye/Service/PalestranteService.java
@@ -1,2 +1,56 @@
-package com.webservice.eventfye.Service;public class PalestranteService {
+package com.webservice.eventfye.Service;
+
+import com.webservice.eventfye.Model.Palestrante;
+import com.webservice.eventfye.Repository.PalestranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PalestranteService {
+
+    @Autowired
+    private PalestranteRepository palestranteRepository;
+
+    public Optional<Palestrante> buscarPalestrantePorNome(String nome) {
+        return palestranteRepository.findByNomeUsuario(nome);
+    }
+
+    public List<Palestrante> buscarPalestrantesPorAreaExpertise(String areaExpertise) {
+        return palestranteRepository.findByAreaExpertisePalestrante(areaExpertise);
+    }
+
+    public List<Palestrante> buscarPalestrantesPorNomeContendo(String nome) {
+        return palestranteRepository.findByNomeUsuarioContaining(nome);
+    }
+
+    public List<Palestrante> buscarPalestrantesPorNomeComecandoCom(String nome) {
+        return palestranteRepository.findByNomeUsuarioStartingWith(nome);
+    }
+
+    public List<Palestrante> buscarTodosPalestrantesOrdenadosPorNome() {
+        return palestranteRepository.findAllByOrderByNomeUsuarioAsc();
+    }
+
+    public long contarPalestrantesPorAreaExpertise(String areaExpertise) {
+        return palestranteRepository.countByAreaExpertisePalestrante(areaExpertise);
+    }
+
+    public Palestrante salvarPalestrante(Palestrante palestrante) {
+        return palestranteRepository.save(palestrante);
+    }
+
+    public void excluirPalestrante(Long id) {
+        palestranteRepository.deleteById(id);
+    }
+
+    public void excluirPalestrantesPorAreaExpertise(String areaExpertise) {
+        List<Palestrante> palestrantes = palestranteRepository.findByAreaExpertisePalestrante(areaExpertise);
+        if (palestrantes.isEmpty()) {
+            throw new RuntimeException("Nenhum palestrante encontrado com a área de expertise fornecida.");
+        }
+        palestranteRepository.deleteAll(palestrantes);
+    }
 }
