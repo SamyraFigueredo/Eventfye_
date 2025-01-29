a3080334030f5abf8675d5c70db28694eb457fb4
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-01-29 00:05:32 -0300

    add listar todos os usuarios em ordem alfabetica

--- src/main/java/com/webservice/eventfye/Repository/UsuarioRepository.java
+++ src/main/java/com/webservice/eventfye/Repository/UsuarioRepository.java
@@ -4,11 +4,13 @@ import com.webservice.eventfye.Model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByNomeUsuario(String nome);
+    List<Usuario> findAllByOrderByNomeUsuarioAsc();
     Optional<Usuario> findByEmailUsuario(String email);
 }
