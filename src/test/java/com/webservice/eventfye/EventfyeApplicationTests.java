9c2402e505104abbacce62f0dfd298e6b402b690
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-01-28 22:12:39 -0300

    add variaveis de ambiente de teste

--- src/test/java/com/webservice/eventfye/EventfyeApplicationTests.java
+++ src/test/java/com/webservice/eventfye/EventfyeApplicationTests.java
@@ -2,12 +2,13 @@ package com.webservice.eventfye;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class EventfyeApplicationTests {
+@ActiveProfiles("test")
+public class EventfyeApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
