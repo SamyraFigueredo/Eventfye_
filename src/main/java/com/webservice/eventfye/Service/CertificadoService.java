d7e9ea3c758005e064f0a9a5ec7588b329592f16
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-01-29 19:41:21 -0300

    creating service for certificate and method for generate pdf

--- src/main/java/com/webservice/eventfye/Service/CertificadoService.java
+++ src/main/java/com/webservice/eventfye/Service/CertificadoService.java
@@ -1,2 +1,40 @@
-package com.webservice.eventfye.Service;public class CertificadoService {
+package com.webservice.eventfye.Service;
+
+import com.webservice.eventfye.Model.Certificado;
+import com.webservice.eventfye.Model.Evento;
+import com.webservice.eventfye.Model.Usuario;
+import com.webservice.eventfye.Service.Utils.PdfGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.ZonedDateTime;
+
+@Service
+public class CertificadoService {
+
+    @Autowired
+    private PdfGenerator pdfGenerator;
+
+    public Certificado gerarCertificado(String nomeUsuario, String nomeEvento, ZonedDateTime dataInicioEvento, ZonedDateTime dataFimEvento) {
+        Usuario usuario = new Usuario();
+        usuario.setNomeUsuario(nomeUsuario);
+
+        Evento evento = new Evento();
+        evento.setNomeEvento(nomeEvento);
+        evento.setDataInicioEvento(dataInicioEvento);
+        evento.setDataFimEvento(dataFimEvento);
+
+        Certificado certificado = Certificado.builder()
+                .nomeUsuario(usuario)
+                .nomeEvento(evento)
+                .dataEmissaoCertificado(java.time.LocalDate.now())
+                .codigoVerificacaoCertificado("codigo_exemplo")
+                .build();
+        try {
+            pdfGenerator.gerarCertificadoPdf(certificado);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return certificado;
+    }
 }
