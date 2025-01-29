11baf397a034711fcd2e6f5cf9385e539e5d552b
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-01-28 23:53:23 -0300

    criando controller para usuario; apenas metodo post

--- src/main/java/com/webservice/eventfye/Controller/UsuarioController.java
+++ src/main/java/com/webservice/eventfye/Controller/UsuarioController.java
@@ -1,4 +1,28 @@
 package com.webservice.eventfye.Controller;
 
-public class UsuarioService {
+import com.webservice.eventfye.Model.Usuario;
+import com.webservice.eventfye.Service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/participante")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<String> criarParticipante(@RequestBody Usuario usuario) {
+        try {
+            usuarioService.salvarUsuario(usuario);
+            return new ResponseEntity<>("Participante criado com sucesso!", HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>("Erro: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao criar participante: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
