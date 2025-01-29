afac032cb79ea58819d51577a58bad8f6ef2c19f
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-01-29 19:45:56 -0300

    add new attributes for certificate issuance

--- src/main/java/com/webservice/eventfye/Model/Certificado.java
+++ src/main/java/com/webservice/eventfye/Model/Certificado.java
@@ -1,11 +1,15 @@
 package com.webservice.eventfye.Model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,4 +27,25 @@ public class Certificado {
 
     @Column(name = "codigo_verificacao_certificado", nullable = false, unique = true)
     private String codigoVerificacaoCertificado;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario nomeUsuario;
+
+    @ManyToOne
+    @JoinColumn(name = "evento_id", nullable = false)
+    private Evento nomeEvento;
+
+    @Column(name = "data_inicio_evento")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssX")
+    private ZonedDateTime dataInicioEvento;
+
+    @Column(name = "data_fim_evento")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssX")
+    private ZonedDateTime dataFimEvento;
+
+    @Column(name = "caminho_arquivo_certificado", nullable = true)
+    private String caminhoArquivoCertificado;
 }
