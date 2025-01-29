b99bc5c7b9dab638574b001808bdd10dfcf839b1
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-01-29 19:39:08 -0300

    creating repository for certificado

--- src/main/java/com/webservice/eventfye/Repository/CertificadoRepository.java
+++ src/main/java/com/webservice/eventfye/Repository/CertificadoRepository.java
@@ -1,2 +1,9 @@
-package com.webservice.eventfye.Repository;public class CertificadoRepository {
+package com.webservice.eventfye.Repository;
+
+import com.webservice.eventfye.Model.Certificado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CertificadoRepository extends JpaRepository<Certificado, Long> {
 }
